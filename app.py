69c310bb2f55f283eb34ce04b7ce2e52d5b325a9
SHENDUDAYE <shendudaye89@gmail.com>
2025-04-17 09:48:47 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import random
+from datetime import datetime, timedelta
+
+st.set_page_config(page_title="Love Match Compatibility Analyzer", layout="wide")
+st.title("🎎 Love Match Compatibility Analyzer")
+
+# ... [Full analysis code goes here] ...
